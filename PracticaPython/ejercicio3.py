fa07a27786fe8249b422a25b8e01b64c450dc9c7
VanesaBustamante <eriannbel@gmail.com>
2026-09-02 19:58:24 -0300

    ejercicicio 3 completo

---
+++ PracticaPython/ejercicio3.py
@@ -0,0 +1,18 @@
+#variable
+frutas = ["naranja" , "pera" , "Mandarina"]
+
+
+#ejercicio
+#frutas.append("banana")               # guardar
+#frutas.insert (0, "palta")            # insertar en cordenada
+#frutas.remove ("pera")                # remove 
+#frutas_eliminada = frutas.pop(2)      # elimuna en cordenada
+#frutas.sort ()                        # ordena en ascendente 
+#frutas.reverse()                       # invierte 
+
+#vista por consola
+
+print ("resultado :"+ str(frutas))
+
+
+
