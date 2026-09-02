ba0345f164f2c07152bbe7f0d89cab220b66d383
VanesaBustamante <eriannbel@gmail.com>
2026-09-02 18:22:13 -0300

    primera practica con python

---
+++ hola.py
@@ -0,0 +1,17 @@
+a = 3.23
+b = 2.23
+c = 2.34
+
+suma = (a+b) * c                               # 1
+suma_es = a + b                                # 2
+son_iguales = a != b                           # 3
+dividido_entero = a // b                       # 4
+resultado_and = (b < 3.23) and (2.23 > c)      # 5
+
+
+print ("1 el resultado es :" + str(resultado_and))
+print ("2 el resultado es :" + str(suma_es))
+print ("3 el resultado es :" + str(son_iguales))
+print ("4 el resultado es :" + str(dividido_entero))
+print ("5 el resultado es :" + str(suma))
+
