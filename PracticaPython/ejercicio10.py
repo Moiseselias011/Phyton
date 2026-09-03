d67e0596c8c4eeb80d390432e5e6890b24ac00a0
VanesaBustamante <eriannbel@gmail.com>
2026-09-03 12:27:23 -0300

    ejercicio 10 completo

---
+++ PracticaPython/ejercicio10.py
@@ -0,0 +1,15 @@
+#nombre = input ("ingrese su nombre: ")
+#edad = input ("ingrese su edad: ")
+
+#print ("hola " + nombre )
+#print ("tu edad es : " + edad +  "años")
+
+#--------------------------------------------------------------------------------------
+
+edad = int(input ("ingrese su edad "))
+
+if edad >= 18:
+    print("puedes votar ")
+else:
+    print ("no puedes votar ")    
+
